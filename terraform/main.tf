8ce29465cdde7748f188548f9044a435ab88e2e5
alerickson0 <24833189+alerickson0@users.noreply.github.com>
2024-04-23 17:03:46 -0700

    feat: Add error page
    Adding folder with my resume and css file. Also, adding code to use
    AWS's CloudFront.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
+locals {
+  s3_origin_id = "resumeS3Origin"
+}
 
 resource "random_string" "bucket_name_add" {
   length           = 16
@@ -18,7 +21,7 @@ resource "aws_s3_bucket" "my_bucket" {
 resource "aws_s3_bucket_website_configuration" "test_site" {
   bucket = aws_s3_bucket.my_bucket.id
   index_document {
-    suffix = "index.html"
+    suffix = "Alvaro_E_resume.html"
   }
   error_document {
     key = "error.html"
@@ -37,12 +40,106 @@ resource "aws_s3_bucket_policy" "public_read_access" {
   policy = data.aws_iam_policy_document.public_read_access.json
 }
 
-# To updload all files present in the "html" folder to my new S3 bucket
+# To upload all files present in the "resume" folder to my new S3 bucket
 resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("html/", "*")
+  for_each      = fileset("resume/", "*")
   bucket        = aws_s3_bucket.my_bucket.id
   key           = each.value
-  source        = "html/${each.value}"
-  etag          = filemd5("html/${each.value}")
+  source        = "resume/${each.value}"
+  etag          = filemd5("resume/${each.value}")
   content_type  = "text/html"
+}
+
+# To updload all files present in the "resume" folder to my new S3 bucket
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront for my resume site"
+  default_root_object = "Alvaro_E_resume.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
