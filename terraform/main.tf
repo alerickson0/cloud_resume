3d17f9c923fcbb1aee770d690bc8995be81df824
alerickson0 <24833189+alerickson0@users.noreply.github.com>
2024-05-23 16:44:53 -0700

    feat: Update resume, css
    Adding JavaScript code and other functionality for a visitor counter on
    my site. Also, fixing JS code errors, adding code for policy to handle
    JS, css files correctly.

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,12 +42,68 @@ resource "aws_s3_bucket_policy" "public_read_access" {
 
 # To upload all files present in the "resume" folder to my new S3 bucket
 resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("resume/", "*")
+  for_each      = fileset("resume/", "**")
   bucket        = aws_s3_bucket.my_bucket.id
   key           = each.value
   source        = "resume/${each.value}"
   etag          = filemd5("resume/${each.value}")
   content_type  = "text/html"
+}
+
+resource "aws_cloudfront_response_headers_policy" "my-custom-javascript-response" {
+  name = "custom-javascript-response"
+
+  custom_headers_config {
+    items {
+      header   = "Content-Type"
+      override = true
+      value    = "text/javascript"
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "my-custom-css-response" {
+  name = "custom-css-response"
+
+  custom_headers_config {
+    items {
+      header   = "Content-Type"
+      override = true
+      value    = "text/css"
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "my_headers_policy" {
+  name = "security-headers-policy"
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+
+    frame_options {
+      override     = true
+      frame_option = "DENY"
+    }
+
+    referrer_policy {
+      override        = true
+      referrer_policy = "same-origin"
+    }
+
+    strict_transport_security {
+      override                   = true
+      access_control_max_age_sec = 63072000
+      include_subdomains         = true
+      preload                    = true
+    }
+
+    xss_protection {
+      override   = true
+      mode_block = true
+      protection = true
+    }
+  }
 }
 
 # To updload all files present in the "resume" folder to my new S3 bucket
@@ -77,13 +133,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    viewer_protocol_policy     = "allow-all"
+    min_ttl                    = 0
+    default_ttl                = 3600
+    max_ttl                    = 86400
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.my_headers_policy.id
   }
 
-  # Cache behavior with precedence 0
+    # Cache behavior with precedence 0
   ordered_cache_behavior {
     path_pattern     = "/content/immutable/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -99,11 +156,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                    = 0
+    default_ttl                = 86400
+    max_ttl                    = 31536000
+    compress                   = true
+    viewer_protocol_policy     = "redirect-to-https"
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.my_headers_policy.id
   }
 
   # Cache behavior with precedence 1
@@ -126,6 +184,41 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.my_headers_policy.id
+  }
+
+  # Cache behavior with precedence 2. For JavaScript handling
+  ordered_cache_behavior {
+    path_pattern     = "*.js"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # Using recommended for S3, Managed-CachingOptimized
+
+    min_ttl                    = 0
+    default_ttl                = 86400
+    max_ttl                    = 31536000
+    compress                   = true
+    viewer_protocol_policy     = "redirect-to-https"
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.my-custom-javascript-response.id
+  }
+
+  # Cache behavior with precedence 3. For css handling
+  ordered_cache_behavior {
+    path_pattern     = "*.css"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # Using recommended for S3, Managed-CachingOptimized
+
+    min_ttl                    = 0
+    default_ttl                = 86400
+    max_ttl                    = 31536000
+    compress                   = true
+    viewer_protocol_policy     = "redirect-to-https"
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.my-custom-css-response.id
   }
 
   price_class = "PriceClass_200"
